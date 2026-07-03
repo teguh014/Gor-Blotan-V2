d94e8509f878237b08683b490a3fb2897f26ddd9
teguh014 <mohammadteguh014@gmail.com>
2026-07-03 13:41:57 +0700

    Trust Railway proxies to fix mixed content

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         // Register custom middleware alias
         $middleware->alias([
             'admin' => EnsureUserIsAdmin::class,
